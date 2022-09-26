53d2e4fb147096f7632956b04b7aaafdffe0a554
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-09-26 14:03:52 +0700

    init

---
+++ src/main/java/com/example/th1/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.th1.repository;
+
+import com.example.th1.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/example/th1/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.th1.service;
+
+import com.example.th1.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
+
